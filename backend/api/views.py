1ee122c9ba7f3c3bd9635c4236ad4a68497ddca0
Aashu2202 <gurjarashish1010@gmail.com>
2024-05-16 23:40:26 -0700

    add

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,7 +17,7 @@ from .assitent import call
 import openai
 
 SALT = "8b4f6b2cc1868d75ef79e5cfb8779c11b6a374bf0fce05b485581bf4e1e25b96c8c2855015de8449"
-URL = "http://localhost:3000"
+URL = "https://interviewrdj-git-main-aashish-yadavs-projects.vercel.app"
 
 def mail_template(content, button_url, button_text):
     return f"""<!DOCTYPE html>
--- frontend/src/pages/Contact/Contact.js
+++ frontend/src/pages/Contact/Contact.js
@@ -23,6 +23,7 @@ const Contact = () => {
             // Send form data to the backend API
             const response = await axios.post(URL, formData);
             console.log(response.data); // Log the response from the backend
+            const data = response.data
             if(data.success===true)
             {
                 alert("form submitted successfully")
@@ -47,18 +48,18 @@ const Contact = () => {
                     <img src={Image_Gif} alt="Contact" className="img-fluid" />
                 </div>
                 <div className="col-lg-6 col-md-6 col-sm-12 mb-4">
-                    <h2 className="mb-4">Contact Us</h2>
+                    <h2 className="mb-4 whitecolor">Contact Us</h2>
                     <form onSubmit={handleSubmit}>
                         <div className="mb-3">
-                            <label htmlFor="name" className="form-label">Name</label>
+                            <label htmlFor="name" className="form-label whitecolor">Name</label>
                             <input type="text" className="form-control contact-input" id="name" name="name" value={formData.name} onChange={handleChange} required />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Email address</label>
+                            <label htmlFor="email" className="form-label whitecolor">Email address</label>
                             <input type="email" className="form-control contact-input" id="email" name="email" value={formData.email} onChange={handleChange} required />
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="message" className="form-label">Message</label>
+                            <label htmlFor="message" className="form-label whitecolor">Message</label>
                             <textarea className="form-control contact-input" id="message" name="message" value={formData.message} onChange={handleChange} rows="4" required></textarea>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
