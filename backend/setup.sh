e1f872e9271c7e64a5db44d9ac9a3a4761a9813d
Aashu2202 <gurjarashish1010@gmail.com>
2024-05-20 08:09:55 -0700

    asdf

--- backend/setup.sh
+++ backend/setup.sh
@@ -3,5 +3,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Install system dependencies for pygobject
+apt-get update
+apt-get install -y libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
+
 # Install Python dependencies
 pip install -r requirements.txt
