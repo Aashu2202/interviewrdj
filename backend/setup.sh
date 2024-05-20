20e2c391f4fe8080835410b7ce713c76a7e2da5e
Aashu2202 <gurjarashish1010@gmail.com>
2024-05-20 08:05:02 -0700

    setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -3,11 +3,5 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Update package lists
-apt-get update
-
-# Install necessary system dependencies
-apt-get install -y pkg-config libgirepository1.0-dev libcairo2-dev libjpeg-dev libgif-dev
-
 # Install Python dependencies
 pip install -r requirements.txt
