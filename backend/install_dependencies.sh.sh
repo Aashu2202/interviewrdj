d56586e893b5ef50b6c8a2ec17e4077d091b8631
Aashu2202 <gurjarashish1010@gmail.com>
2024-05-22 11:18:39 +0530

    sh delete

--- backend/install_dependencies.sh.sh
+++
@@ -1,9 +0,0 @@
-# backend/install_dependencies.sh
-apt-get update && apt-get install -y \
-    libffi-dev \
-    libgirepository1.0-dev \
-    pkg-config \
-    libcairo2-dev \
-    gcc \
-    g++ \
-    make
